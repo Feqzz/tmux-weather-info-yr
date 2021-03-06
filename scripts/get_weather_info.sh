f17c5ae7dde995555910b44e1a7bca4d4263b6a8
Feqzz <stian.onarheim@protonmail.com>
2021-03-06 11:27:42 +0100

    The plugin will now only query YR and ip-api every 10 minutes

--- scripts/get_weather_info.sh
+++ scripts/get_weather_info.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+CACHE_OUTPUT=/tmp/weather-result-cache.txt
+AGE_TO_CACHE="600" #10 minutes
 
+if [ ! -f "$CACHE_OUTPUT" ] || [ $(($(date +%s) - $(stat --format=%Y "$CACHE_OUTPUT"))) -le "$AGE_TO_CACHE" ] 
+then
+	cat "$CACHE_OUTPUT"
+	exit 0
+fi
+(
 LATITUDE=$(curl -s 'http://ip-api.com/json/' | jq -r '.lat')
 LONGITUDE=$(curl -s 'http://ip-api.com/json/' | jq -r '.lon')
 CITY=$(curl -s 'http://ip-api.com/json/' | jq -r '.city')
 
 YR="curl -s 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat="$LATITUDE"&lon="$LONGITUDE"' | jq -r '.properties.timeseries[0].data.instant.details.air_temperature'"
-
 SYMBOL_QUERY="curl -s 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat="$LATITUDE"&lon="$LONGITUDE"' | jq -r '.properties.timeseries[0].data.next_1_hours.summary.symbol_code'"
 TEMPERATURE=$(eval $YR)
 TEMP_INTEGER=${TEMPERATURE%.*}
@@ -39,5 +46,4 @@ case $SYMBOL in
 esac
 
 printf "%s\n" "$SYMBOL $TEMP_INTEGER°C"
-
-
+) > $CACHE_OUTPUT
